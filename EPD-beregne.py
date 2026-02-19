cac85a0f4eec2df4c578dd791f837d1bd5e5be63
lkivers1-dk <lkivers1@gmail.com>
2026-02-19 17:11:08 +0100

    Add files via upload

--- EPD-beregne.py
+++ EPD-beregne.py
@@ -138,10 +138,8 @@ if st.button("Beregn"):
 
     st.markdown("---")
 
-    st.write(f"Der er regnet med transportafstand til byggeplads på "
-        {afstand:.1f} " km")
-    st.write(f"En samlet CO2 belastning på "
-        {samlet_belastning:.3e} " kg CO₂e/m²")
-    st.write(f"og en samlet tykkelse på " {samlet_tykkelse_m:.3f} " .")
+    st.write(f"Der er regnet med transportafstand til byggeplads på {afstand:.1f} km")
+    st.write(f"En samlet CO2 belastning på {samlet_belastning:.3e} kg CO₂e/m²")
+    st.write(f"og en samlet tykkelse på {samlet_tykkelse_m:.3f}.")
         
-    )
+    
